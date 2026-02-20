2992d5b4ab778b32b42fa88270c5a9a2b2e30760
Numpang <thenumpang@gmail.com>
2026-02-21 00:18:59 +0700

    Added 17 Sample Questions

---
+++ src/lib/assets/questionnaires.ts
@@ -0,0 +1,206 @@
+export const questionnaires = {
+    'questionnaire-1': {
+        id: '1',
+        message: 'Car',
+        answers: [
+            {
+                answer1: '連',
+                answer2: '車',
+                answer3: '東',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-2': {
+        id: '2',
+        message: 'How are you?',
+        answers: [
+            {
+                answer1: 'お元気ですか？',
+                answer2: 'お久しぶりです！',
+                answer3: 'こんにちは！',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-3': {
+        id: '3',
+        message: 'Where did you go?',
+        answers: [
+            {
+                answer1: 'どこに行くの？',
+                answer2: 'どこに行くつもりですか？',
+                answer3: 'どこに行ったの？',
+            }
+        ],
+        correct_answer: 'answer3'
+    },
+    'questionnaire-4': {
+        id: '4',
+        message: 'I work in Japan',
+        answers: [
+            {
+                answer1: '日本で働きます！',
+                answer2: '日本に行きました！',
+                answer3: '日本で勉強します！',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-5': {
+        id: '5',
+        message: 'Variable (In Coding)',
+        answers: [
+            {
+                answer1: '変数',
+                answer2: '関数',
+                answer3: '配列',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-6': {
+        id: '6',
+        message: 'What is your name?',
+        answers: [
+            {
+                answer1: '君の名は？',
+                answer2: '僕の名は？',
+                answer3: '彼の名は？',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-7': {
+        id: '7',
+        message: 'Deadline',
+        answers: [
+            {
+                answer1: '納期',
+                answer2: '管理',
+                answer3: '可用性',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-8': {
+        id: '8',
+        message: 'Management',
+        answers: [
+            {
+                answer1: '管理',
+                answer2: '担当',
+                answer3: '予算',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-9': {
+        id: '9',
+        message: 'UI',
+        answers: [
+            {
+                answer1: 'ユーザーエクスペリエンス',
+                answer2: 'ユーザーインターフェース',
+                answer3: 'ユーザーサービス',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-10': {
+        id: '10',
+        message: 'Report Document',
+        answers: [
+            {
+                answer1: '設計書',
+                answer2: '報告書',
+                answer3: '見積書',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-11': {
+        id: '11',
+        message: 'It is completed',
+        answers: [
+            {
+                answer1: '了解です！',
+                answer2: '確認しました！',
+                answer3: '完了しました！',
+            }
+        ],
+        correct_answer: 'answer3'
+    },
+    'questionnaire-12': {
+        id: '12',
+        message: 'High Priority',
+        answers: [
+            {
+                answer1: '高優先',
+                answer2: '低優先',
+                answer3: '中優先',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-13': {
+        id: '13',
+        message: 'Start',
+        answers: [
+            {
+                answer1: '成功',
+                answer2: '開始',
+                answer3: '緊急',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-14': {
+        id: '14',
+        message: 'End',
+        answers: [
+            {
+                answer1: '終了',
+                answer2: '失敗',
+                answer3: '合間',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-15': {
+        id: '15',
+        message: 'Ready for Deployment',
+        answers: [
+            {
+                answer1: 'デプロイ準備完了',
+                answer2: 'デプロイ準備開始',
+                answer3: 'デプロイ準備中',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-16': {
+        id: '16',
+        message: 'What is your favorite color?',
+        answers: [
+            {
+                answer1: 'あの色がきれいだろう？',
+                answer2: '好きな色は何ですか？',
+                answer3: 'この色が好きですか？',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-17': {
+        id: '17',
+        message: 'I like you',
+        answers: [
+            {
+                answer1: '君が好きです！',
+                answer2: '月がきれいですね！',
+                answer3: '私を結婚してください！',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+}
