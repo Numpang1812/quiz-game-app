688909d7194218ccaa9667ce6dead19fd0419798
Numpang <thenumpang@gmail.com>
2026-02-24 23:19:10 +0700

    Added more questions

--- src/lib/assets/questionnaires.ts
+++ src/lib/assets/questionnaires.ts
@@ -220,4 +220,247 @@ export const questionnaires = {
         ],
         correct_answer: 'answer1'
     },
+    'questionnaire-18': {
+        id: '18',
+        message: 'Hair',
+        answers: [
+            {
+                answer1: 'かみ（神）',
+                answer2: 'かみ（紙）',
+                answer3: 'かみ（加味）',
+                answer4: 'かみ（髪）',
+            }
+        ],
+        correct_answer: 'answer4'
+    },
+    'questionnaire-19': {
+        id: '19',
+        message: 'Bug (In Coding)',
+        answers: [
+            {
+                answer1: 'バグ',
+                answer2: 'バッグ',
+                answer3: 'ボッグ',
+                answer4: 'ボグ',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-20': {
+        id: '20',
+        message: "Let's go to the park",
+        answers: [
+            {
+                answer1: '公園に行くよ',
+                answer2: '公園に行こうぜ',
+                answer3: '公園に行くな',
+                answer4: '公園に行きたい',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-21': {
+        id: '21',
+        message: 'Although it is raining, I will go to the park',
+        answers: [
+            {
+                answer1: '雨が降ってるのに、公園に行かない。',
+                answer2: '雨が降ってるので、公園に行きます。',
+                answer3: '雨が降ってるのは、公園に行きます。',
+                answer4: '雨が降ってるのに、公園に行きます。',
+            }
+        ],
+        correct_answer: 'answer4'
+    },
+    'questionnaire-22': {
+        id: '22',
+        message: 'I come from Japan',
+        answers: [
+            {
+                answer1: '私は日本人です。',
+                answer2: '私は日本から来ました。',
+                answer3: '私は日本人じゃない。',
+                answer4: '私は日本から来なかった。',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-23': {
+        id: '23',
+        message: 'I play football on the weekends',
+        answers: [
+            {
+                answer1: '週末にサッカーをやるぞ。',
+                answer2: '今週にサッカーをやるぞ。',
+                answer3: '週末にサッカーをやるな。',
+                answer4: '週末にサッカーをやめる。',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-24': {
+        id: '24',
+        message: 'The game will begin!',
+        answers: [
+            {
+                answer1: '試合開始！',
+                answer2: '勝負開始！',
+                answer3: '練習開始！',
+                answer4: '試験開始！',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+    'questionnaire-25': {
+        id: '25',
+        message: "Don't forget to do your homework!",
+        answers: [
+            {
+                answer1: '宿題を忘れてもいいよ！',
+                answer2: '宿題を忘れないでね！',
+                answer3: '宿題を忘れちゃった！',
+                answer4: '宿題を忘れなければならない！',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+    'questionnaire-26': {
+        id: '26',
+        message: 'I will call you later.',
+        answers: [
+            {
+                answer1: 'あとで電話するね。',
+                answer2: 'あとで電話して。',
+                answer3: 'あとで電話するな。',
+                answer4: '電話したほうがいい。',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+
+    'questionnaire-27': {
+        id: '27',
+        message: 'Please close the window.',
+        answers: [
+            {
+                answer1: '窓を開けてください。',
+                answer2: '窓を閉めてください。',
+                answer3: '窓が閉まっています。',
+                answer4: '窓を閉めました。',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+
+    'questionnaire-28': {
+        id: '28',
+        message: 'It is raining now.',
+        answers: [
+            {
+                answer1: '今、晴れています。',
+                answer2: '今、雨が降っていません。',
+                answer3: '今、雨が降っています。',
+                answer4: '今、雨でしょう。',
+            }
+        ],
+        correct_answer: 'answer3'
+    },
+
+    'questionnaire-29': {
+        id: '29',
+        message: 'I have already eaten.',
+        answers: [
+            {
+                answer1: 'まだ食べていません。',
+                answer2: 'もう食べました。',
+                answer3: 'これから食べます。',
+                answer4: '毎日食べます。',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+
+    'questionnaire-30': {
+        id: '30',
+        message: 'Can you help me?',
+        answers: [
+            {
+                answer1: '手伝ってくれますか？',
+                answer2: '手伝いましょうか？',
+                answer3: '手伝ってくれません。',
+                answer4: '手伝ってもいいですよ。',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+
+    'questionnaire-31': {
+        id: '31',
+        message: 'I forgot my wallet.',
+        answers: [
+            {
+                answer1: '財布を忘れる？',
+                answer2: '財布を忘れました。',
+                answer3: '財布を忘れないでね。',
+                answer4: '財布を捨てました。',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+
+    'questionnaire-32': {
+        id: '32',
+        message: 'Let’s study together.',
+        answers: [
+            {
+                answer1: '一緒に勉強しよう。',
+                answer2: '一緒に勉強した。',
+                answer3: '一緒に勉強しない。',
+                answer4: '一緒に勉強したい。',
+            }
+        ],
+        correct_answer: 'answer1'
+    },
+
+    'questionnaire-33': {
+        id: '33',
+        message: 'I am looking for my keys.',
+        answers: [
+            {
+                answer1: '鍵を見つけました。',
+                answer2: '鍵を探しています。',
+                answer3: '鍵を忘れています',
+                answer4: '鍵を見てます。',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+
+    'questionnaire-34': {
+        id: '34',
+        message: 'Please wait here.',
+        answers: [
+            {
+                answer1: 'ここで持ってください。',
+                answer2: 'ここで待ってください。',
+                answer3: 'ここに触ってください。',
+                answer4: 'ここで立ってください。',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
+
+    'questionnaire-35': {
+        id: '35',
+        message: 'I can’t understand.',
+        answers: [
+            {
+                answer1: '分かりにくい。',
+                answer2: '分かりません。',
+                answer3: '分かりやすい。',
+                answer4: '分かっていました。',
+            }
+        ],
+        correct_answer: 'answer2'
+    },
 }
